c13a954ef761a3e41ce0889c88d4a95813404c97
Ashfak Niloy <ashfakniloy56@gmail.com>
2023-01-05 15:08:32 +0600

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,19 +4,23 @@ import NextNProgress from "nextjs-progressbar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}>
-      <NextNProgress options={{ showSpinner: false }} />
-      <ToastContainer
-        className="mt-[72px] lg:mt-[52px] z-10"
-        autoClose={1500}
-      />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionProvider>
+    <>
+      <SessionProvider session={session}>
+        <NextNProgress options={{ showSpinner: false }} />
+        <ToastContainer
+          className="mt-[72px] lg:mt-[52px] z-10"
+          autoClose={1500}
+        />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
+      <Analytics />
+    </>
   );
 }
 
